1663295e883718f392b6d3a9f663f343edaf0cbb
hoangziet <hoangziet22@gmail.com>
2025-04-23 16:51:42 +0700

    update swiperjs for projects

---
+++ script.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".mySwiper", {
+        pagination: {
+            el: ".swiper-pagination",
+            type: "fraction",
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
+        },
+        loop: true, 
+    });
+});
